65eb03afe71943de7ad5358eac9c7306b093e53b
abby <79733013+abbyinkr@users.noreply.github.com>
2024-03-19 13:13:08 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,12 +1,3 @@
 def solution(my_string):
-    
-    alpha_list = [i for i in range(ord('A'), ord('Z') + 1)]  # 대문자 A~Z
-    alpha_list += [i for i in range(ord('a'), ord('z') + 1)]  # 소문자 a~z
-    alpha_dict = {ord: 0 for ord in alpha_list}
-    ord_list = [ord(char) for char in my_string]
-    
-    for ord_char in ord_list:
-        alpha_dict[ord_char] += 1
-        
-    return list(alpha_dict.values())
+    return [my_string.count(chr(i)) for i in range(ord('A'), ord('Z')+1)] + [my_string.count(chr(i)) for i in range(ord('a'), ord('z')+1)]
         
