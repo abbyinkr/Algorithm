20fc502d3cfe8f5565819fb2cf9e779dcbc51b69
abby <79733013+abbyinkr@users.noreply.github.com>
2024-03-24 00:42:45 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,10 +1,2 @@
 def solution(myString):
-    arr = []
-    for c in myString:
-        if c == "a":
-            arr.append("A")
-        elif c != "A" and c.isupper():
-            arr.append(c.lower())
-        else:
-            arr.append(c)
-    return ''.join(arr)
+    return myString.lower().replace('a', 'A')
