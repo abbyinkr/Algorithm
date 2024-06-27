6a6031515c9f32caccd094fc7454797dac630be7
abby <79733013+abbyinkr@users.noreply.github.com>
2024-06-27 18:29:30 +0900

    [level 0] Title: 배열 만들기 6, Time: 16.85 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    stk = []
+    for i in range(len(arr)):
+        if not stk:
+            stk.append(arr[i]) 
+        elif stk and stk[-1] == arr[i]:
+            stk.pop()
+        elif stk and stk[-1] != arr[i]:
+            stk.append(arr[i]) 
+    return stk if stk else [-1]
