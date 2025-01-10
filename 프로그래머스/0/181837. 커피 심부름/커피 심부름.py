597463bf0a010059f23327078ef6005f199f1784
ahbyeol <79733013+abbyinkr@users.noreply.github.com>
2025-01-10 17:31:18 +0900

    [level 0] Title: 커피 심부름, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -1,11 +1,9 @@
 def solution(order):
     sum = 0
     for drink in order:
-        if "americano" in drink or "anything" == drink:
-            sum += 4500
-            continue
         if "cafelatte" in drink:
             sum += 5000
             continue
+        sum += 4500
     return sum
         
