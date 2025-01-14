47cfad6c7baae4ca2b1b973762d7e1869c6afe5c
ahbyeol <79733013+abbyinkr@users.noreply.github.com>
2025-01-14 16:47:02 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,12 @@
+SELECT SCORE, E.EMP_NO, EMP_NAME, POSITION, EMAIL
+FROM HR_EMPLOYEES E
+JOIN
+    (
+        SELECT  EMP_NO,
+                SUM(SCORE) "SCORE"
+                FROM HR_GRADE 
+                GROUP BY EMP_NO
+                ORDER BY SCORE DESC
+                LIMIT 1
+    ) G
+ON E.EMP_NO = G.EMP_NO;
