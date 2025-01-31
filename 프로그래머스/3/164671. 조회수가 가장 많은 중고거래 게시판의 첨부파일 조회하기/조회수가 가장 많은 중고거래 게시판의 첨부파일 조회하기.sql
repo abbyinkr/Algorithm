4536e13a6295baafc0a9ad00fb336f2fec9aa028
ahbyeol <79733013+abbyinkr@users.noreply.github.com>
2025-01-31 15:38:40 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,12 +1,20 @@
+WITH MOST_VIEWED AS 
+    (
+        SELECT *
+        FROM USED_GOODS_BOARD
+        ORDER BY VIEWS DESC
+        LIMIT 1
+    )
 SELECT 
-    CONCAT('/home/grep/src/', B.BOARD_ID, '/', F.FILE_ID, F.FILE_NAME, F.FILE_EXT) FILE_PATH
-FROM 
-(
-    SELECT *
-    FROM USED_GOODS_BOARD
-    ORDER BY VIEWS DESC
-    LIMIT 1
-) B
+    CONCAT(
+            '/home/grep/src/', 
+            MOST_VIEWED.BOARD_ID, 
+            '/', 
+            F.FILE_ID, 
+            F.FILE_NAME, 
+            F.FILE_EXT
+    ) FILE_PATH
+FROM MOST_VIEWED
     JOIN USED_GOODS_FILE F
-    ON B.BOARD_ID = F.BOARD_ID
+    ON MOST_VIEWED.BOARD_ID = F.BOARD_ID
 ORDER BY FILE_ID DESC;
