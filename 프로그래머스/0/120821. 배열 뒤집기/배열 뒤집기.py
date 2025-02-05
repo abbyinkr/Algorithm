44476da06853a42e136c96013e053cdfe5b042cf
ahbyeol <79733013+abbyinkr@users.noreply.github.com>
2025-02-05 21:44:01 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.py
@@ -1,6 +1,2 @@
 def solution(num_list):
-    answer = []
-    length = len(num_list)
-    for i in range(length):
-        answer.append(num_list[length-i-1])
-    return answer
+    return num_list[::-1]
